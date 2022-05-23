785e557f348a2bac98deb11e0fdf07b123f953ef
nataromrom <nata976@gmail.com>
2022-05-23 17:18:16 +0300

    Update homework

--- Week 16/Task1/assets/js/main.js
+++ Week 16/Task1/assets/js/main.js
@@ -3,23 +3,26 @@ sumInput();
 function sumInput() {
     let userArray = [];
     let i = 0;
+    let sum = 0;
     while (true) {
         let item = prompt ("Введите число");
             if (!item || item ==="" || isNaN(item)) {
                 break;
             };
         userArray[i] = +item;
+        sum +=  +userArray[i];
         i++;
-    };
 
+    };
+    console.log(sum);
     if  (userArray.length > 0) {
         document.querySelector(".array").innerHTML = `Отcортированный массив: ${userArray.sort(function (a,b) {
             return a - b })}`;
         document.querySelector(".count").innerHTML = `Количество элементов в массиве: ${userArray.length}`;
+        document.querySelector(".sum").innerHTML = `Сумма элементов массива: ${sum}`;
     } else {
         document.querySelector(".array").innerHTML = `В массиве нет элементов`;
     }
-/*     console.log(userArray.sort(function (a,b) {
-        return a - b }), `Количество элементов в массиве: ${userArray.length}`);   */
+
 }
 
