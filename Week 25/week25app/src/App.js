bf44d605ba09d62787f337a34a636143fa518b0d
nataromrom <nata976@gmail.com>
2022-08-07 18:28:45 +0300

    Homework week25

---
+++ Week 25/week25app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Chart from './Chart';
+function App() {
+    return(
+        <div  className='App'>
+            <h1>Chart</h1>
+            <Chart />
+        </div>
+    );
+}
+
+export default App;
